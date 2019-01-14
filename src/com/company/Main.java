308cbbb44b742df578e5f91e9ee898be9b7dc3a8
ldagit <litvintsova@gmail.com>
2019-01-14 22:23:37 +0300

    Удаление пробелов из строки без использования String Buffer

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String Str = in.nextLine();
+        String[] StrParts = Str.split(" ");
+
+        int i;
+        String NewStr = new String();
+        for (i=0; i<StrParts.length; ++i){
+            if (!StrParts[i].equals(" ")){
+                NewStr = NewStr.concat(StrParts[i]);
+            }
+        }
+        System.out.println("Строка без пробелов: " + NewStr);
+    }
+}
