a47455319fb494f5a5e9b2fa5970c6cd22c99a47
ldagit <litvintsova@gmail.com>
2019-01-14 23:44:03 +0300

    Через метод replace

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,6 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку: ");
         String Str = in.nextLine();
-        String[] StrParts = Str.split(" ");
-
-        int i;
-        String NewStr = new String();
-        for (i=0; i<StrParts.length; ++i){
-           NewStr = NewStr.concat(StrParts[i]);
-        }
-        System.out.println("Строка без пробелов: " + NewStr);
+        System.out.println("Строка без пробелов: " + Str.replace(" ", ""));
     }
 }
